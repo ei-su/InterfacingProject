57a14cff055b1d2d623e9741d8a4ca0e1e97fa42
RyanMB13 <125330911+RyanMB13@users.noreply.github.com>
2024-12-01 21:46:06 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-extern void x64SAXPYfunction(long long int n, float a, float* x, float* y, float* z);
+extern void x64SAXPYfunction(long long int n, float* x, float* y, float* z, float a);
 
 void cLangSAXPYfunction(long long int n, float a, float* x, float* y, float* z) {
 	int i;
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		z[i] = a * x[i] + y[i];
 }
 
 int main() {
-	unsigned long long int ARRAY_SIZE = 536870912;
+	unsigned long long int ARRAY_SIZE = 1048576; // 2^20
+	//unsigned long long int ARRAY_SIZE = 16777216; // 2^24
+	//unsigned long long int ARRAY_SIZE = 1073741824; // 2^30
 	unsigned long long int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
 	// timer variable
 	clock_t start, end;
@@ -52,24 +54,27 @@ int main() {
 	time_taken = ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
 	printf("Time (in C, as ms) = %lf ms\n", time_taken);
 	// sanity check by printing
-	for (int i = 0; i < 10; i++)
-		printf("new value of x[%d] in C = %f\n", i, z[i]);
+	for (int i = 0; i < 5; i++) {
+	printf("new value of Z[%d] in C = %f\n", i, z[i]);
+	}
 
 	//// ~~~~~~~~~~ x86-64 ~~~~~~~~~~
 	//// re-initialize array
-	//for (int i = 0; i < ARRAY_SIZE; i++) {
-	//	x[i] = 0.0;
-	//	y[i] = 0.0;
-	//	z[i] = 0.0;
-	//}
-	//// in assembly
-	//start = clock();
-	//x64SAXPYfunction(ARRAY_SIZE, a, x, y, z);
-	//end = clock();
-	//time_taken = ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
-	//printf("Time (in ASM, as ms) = %lf ms\n", time_taken);
-	//// sanity check by printing
-	//for (int i = 0; i < 10; i++)
-	//	printf("new value of x[%d] in asm = %f\n", i, x[i]);
-	return 0;
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		x[i] = 5.0;
+		y[i] = 5.0;
+		z[i] = 0.0;
+	}
+		//// in assembly
+		start = clock();
+		x64SAXPYfunction(ARRAY_SIZE, x, y, z, a);
+		end = clock();
+		time_taken = ((double)(end - start) * 1000 / CLOCKS_PER_SEC);
+		printf("\nTime (in ASM, as ms) = %lf ms\n", time_taken);
+		//// sanity check by printing
+		for (int i = 0; i < 5; i++) {
+			printf("new value of Z[%d] in asm = %f\n", i, z[i]);
+		}
+	
+		return 0;
 }
