4bc300d1a8f1d838afd43eea9d267cf32a815d8a
RyanMB13 <125330911+RyanMB13@users.noreply.github.com>
2024-12-01 21:46:58 +0800

    Update x64main.asm

--- x64main.asm
+++ x64main.asm
@@ -1,5 +1,3 @@
-section .data
-
 section .text
 bits 64
 default rel
@@ -7,6 +5,22 @@ default rel
 global x64SAXPYfunction
 
 x64SAXPYfunction:
-; write code here
+    push rbp
+    mov rbp, rsp
+    add rbp, 16
+    xor rbx, rbx                   ; counter rbx = i
+   
+L1:
+    movss xmm1, [rbp + 32]         ; xmm1 takes a
+    movss xmm2, [rdx + rbx]        ; xmm2 takes x[i]
+    movss xmm3, [r8 + rbx]         ; xmm3 takes y[i]
 
-ret
+    mulss   xmm2, xmm1             ; xmm2 = a * x[i]
+    addss   xmm2, xmm3             ; xmm2 = (a * x[i]) + y[i]
+    movss   [r9 + rbx], xmm2       ; move result to 5th param (z)
+ 
+    add rbx, 4
+    loop      L1                   ; loop until RCX is zero  
+    
+    pop rbp
+    ret
